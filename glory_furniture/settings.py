24b19859abaa95c2fd4a61cf3d8e7aabeb70ab51
Ganes <ganesh@gloryfurniture.com>
2026-09-19 13:45:01 +0530

    Use CompressedStaticFilesStorage in STORAGES for WhiteNoise

--- glory_furniture/settings.py
+++ glory_furniture/settings.py
@@ -190,7 +190,7 @@ if USE_CLOUDINARY:
             'BACKEND': 'cloudinary_storage.storage.MediaCloudinaryStorage',
         },
         'staticfiles': {
-            'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+            'BACKEND': 'whitenoise.storage.CompressedStaticFilesStorage',
         },
     }
 
